e0d23c74f051486f696e850efbf0a8f3cc5f9c6c
takuan0926 <s23c1076vc@s.chibakoudai.jp>
2025-01-03 21:43:18 +0900

    Update time_publisher.py

--- current_time_publisher/time_publisher.py
+++ current_time_publisher/time_publisher.py
@@ -7,12 +7,12 @@ class TimePublisher(Node):
     def __init__(self):
         super().__init__('time_publisher')
         self.publisher_ = self.create_publisher(String, 'current_time', 10)
-        self.timer = self.create_timer(1.0, self.publish_current_time)  # 1秒ごとに時刻を配信
+        self.timer = self.create_timer(1.0, self.publish_current_time)  #1秒ごとに時刻を配信
         self.get_logger().info('Time Publisher Node has started.')
 
     def publish_current_time(self):
         msg = String()
-        msg.data = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # 現在時刻をフォーマットしてメッセージにセット
+        msg.data = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  #現在時刻をフォーマットしてメッセージにセット
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
 
